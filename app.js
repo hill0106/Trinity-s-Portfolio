cc1d0c4484fbcb0b41bc0c80d4ba17ad4b525a5c
hill0106 <trinity260935@gmail.com>
2022-03-27 04:37:36 +0800

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+// const text = document.querySelectorAll(".thePaths");
+// var num = text.getTotalLength();
+// console.log(num);
+
+
+// const lastWord = document.querySelector("#first");
+// //console.log(lastWord);
+// const animation = document.querySelector("div.animation");
+// lastWord.addEventListener('animationed' , () => {
+//     animation.style="transition: all 1 ease; opacity:0; pointer-events: none;";
+// });
+let c = document.querySelector("body");
+console.log(c.childNodes);
+console.log(c.children);
